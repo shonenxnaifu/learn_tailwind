7fea37da0cd6cd26895021e1156c6df0f4ac0b81
Pawitra Warda <pawitrawarda@gmail.com>
2022-09-19 11:20:50 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      spacing: {
+        13: '3.25rem'
+      },
+      fontFamily: {
+        inter:['Inter']
+      },
+      colors: {
+        wpu: '#bada55',
+        wpudua: '#c0ffee'
+      }
+    },
+  },
+  plugins: [],
+}
