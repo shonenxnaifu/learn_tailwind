e20c28fdf25a874bb539060813857ebf13543a35
Pawitra Warda <pawitrawarda@gmail.com>
2022-09-21 00:27:43 +0700

    transform transition

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,16 @@ module.exports = {
       colors: {
         wpu: '#bada55',
         wpudua: '#c0ffee'
+      },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+        'wiggle': 'wiggle 1s ease-in-out infinite',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%' : { transform: 'rotate (-3deg)' },
+          '50%': { transform: 'rotate(3deg)'}
+        }
       }
     },
   },
